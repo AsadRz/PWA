cc3de35c74919de73e4ae20c250cbead62892cba
Asad Ullah Riaz <asadullahriaz7@gmail.com>
2021-02-16 20:04:25 +0500

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+const About = lazy(() => import('./About'));
+const Home = lazy(() => import('./Home'));
+
+const App: React.FC = () => (
+  <Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path='/about'>
+          <About />
+        </Route>
+        <Route path='/'>
+          <Home />
+        </Route>
+      </Switch>
+    </Suspense>
+  </Router>
+);
+
+export default App;
---
+++ src/Home.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Home: React.FC = () => <h1>Welcome to your PWA!</h1>;
+
+export default Home;
